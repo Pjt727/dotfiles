e1ea1fdce14add68d0e10b8568c7a8fd9c53b496
Patrick <Patrick@nytyler.com>
2025-05-01 11:15:22 +0900

    MCPhub

--- nvim/.config/nvim/after/plugin/codecompanion.lua
+++ nvim/.config/nvim/after/plugin/codecompanion.lua
@@ -76,6 +76,16 @@ require("codecompanion").setup({
 			end,
 		},
 	},
+	extensions = {
+		mcphub = {
+			callback = "mcphub.extensions.codecompanion",
+			opts = {
+				show_result_in_chat = true, -- Show mcp tool results in chat
+				make_vars = true, -- Convert resources to #variables
+				make_slash_commands = true, -- Add prompts as /slash commands
+			},
+		},
+	},
 })
 
 vim.keymap.set("n", "<Leader>h", "<CMD>CodeCompanionChat Toggle<CR>", { desc = "Open Chat GTP" })
--- zshrc/.zshrc
+++ zshrc/.zshrc
@@ -89,3 +89,11 @@ eval "$(zoxide init zsh)"
 #
 # bun completions
 [ -s "/home/pjt727/.bun/_bun" ] && source "/home/pjt727/.bun/_bun"
+
+NPM_PACKAGES="${HOME}/.npm-packages"
+
+export PATH="$PATH:$NPM_PACKAGES/bin"
+
+# Preserve MANPATH if you already defined it somewhere in your config.
+# Otherwise, fall back to `manpath` so we can inherit from `/etc/manpath`.
+export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
